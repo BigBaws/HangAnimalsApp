65eab1382d0b0820e7bce14a9cfe0a7566045927
Silas El-Azm Stryhn <silas_es29@hotmail.com>
2017-05-03 14:03:35 +0200

    added some new async classes

---
+++ app/src/main/java/com/bigbaws/hanganimals/backend/asynctasks/LogicAsync.java
@@ -0,0 +1,26 @@
+package com.bigbaws.hanganimals.backend.asynctasks;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Silas on 03-05-2017.
+ */
+
+public class LogicAsync extends AsyncTask<String, String, String> {
+
+
+    @Override
+    protected String doInBackground(String... params) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+
+    }
+}
